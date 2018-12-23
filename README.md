d7b47cdf76914bb075052833ef0a7ca9f896a3b8
joe <394054470@qq.com>
2018-12-23 23:11:10 +0800

    init project

---
+++ README.md
@@ -0,0 +1,12 @@
+# Rain
+Maybe life is a journey.This project create on a rainy night.
+
+一个基于springboot搭建的分布式系统，主要用于计算和图片处理。
+
+# part1
+## module bigdata
+    1.搭建hadoop分布式集群环境（先不采用容器形式，不考虑ha），主要涉及hdfs，yarn，mapreduce。最后提供一键式部署脚本。
+    2.分装大数据相关接口
+    
+# part2
+## module ai
---
+++ src/main/java/rain/dsys/RainApplication.java
@@ -0,0 +1,28 @@
+package rain.dsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+@Controller
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class RainApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainApplication.class, args);
+	}
+
+}
+
