ea1fdee5d314b295d380b610cad0272dc409b3ca
joe <394054470@qq.com>
2018-12-27 00:02:37 +0800

    add docker zk shell scripts

---
+++ src/main/java/rain/dsys/scripts/zookeeper.sh
@@ -0,0 +1,3 @@
+docker run --name zk1 --hostname zoo1 -d --env ZOO_MY_ID=1 --env "ZOO_SERVERS=server.1=0.0.0.1:2888:3888 server.2=zoo2::3888 server.3=zoo3:2888:3888" zookeeper
+docker run --name zk2 --hostname zoo2 -d --env ZOO_MY_ID=2 --env "ZOO_SERVERS=server.1=zoo1:2888:3888 server.2=0.0.0.1:2888:3888 server.3=zoo3:2888:3888" zookeeper
+docker run --name zk3 --hostname zoo3 -d --env ZOO_MY_ID=3 --env "ZOO_SERVERS=server.1=zoo1:2888:3888 server.2=zoo2:2888:3888 server.3=0.0.0.1:2888:3888" zookeeper
