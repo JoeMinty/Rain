b7512beb1710d0fb8f1786eada8ac5a16fb7ff52
Allen <985272306@qq.com>
2019-01-07 18:20:30 +0800

    first commit

---
+++ src/main/java/rain/dsys/common/QuartzUtil/QuartzUtil.java
@@ -0,0 +1,122 @@
+package rain.dsys.common.QuartzUtil;
+
+import java.util.UUID;
+
+import io.netty.util.internal.StringUtil;
+import rain.dsys.common.QuartzJobs.MyJob1;
+
+/**
+ * Quartz工具类
+ * 
+ * @author Allen
+ */
+public class QuartzUtil {
+
+	// 如果不传参的话为QuartzUtil创建一个默认的job
+	public static String JOB_NAME = "动态任务调度";
+	public static String TRIGGER_NAME = "动态任务触发器";
+	public static String JOB_GROUP_NAME = "XLXXCC_JOB_GROUP";
+	public static String TRIGGER_GROUP_NAME = "XLXXCC_JOB_GROUP";
+
+	public QuartzUtil() {
+		this(JOB_NAME, UUID.randomUUID().toString(), UUID.randomUUID().toString(), UUID.randomUUID().toString());
+	}
+
+	public QuartzUtil(String jOB_NAME2) {
+		this(jOB_NAME2, UUID.randomUUID().toString(), UUID.randomUUID().toString(), UUID.randomUUID().toString());
+	}
+
+	public QuartzUtil(String jOB_NAME3, String tRIGGER_NAME3, String jOB_GROUP_NAME3, String tRIGGER_GROUP_NAME3) {
+		if (StringUtil.isNullOrEmpty(jOB_NAME3)) {
+			throw new NullPointerException("JOB_NAME 为空");
+		}
+		if (StringUtil.isNullOrEmpty(tRIGGER_NAME3)) {
+			throw new NullPointerException("tRIGGER_NAME 为空");
+		}
+		if (StringUtil.isNullOrEmpty(jOB_GROUP_NAME3)) {
+			throw new NullPointerException("jOB_GROUP_NAME 为空");
+		}
+		if (StringUtil.isNullOrEmpty(tRIGGER_GROUP_NAME3)) {
+			throw new NullPointerException("tRIGGER_GROUP_NAME 为空");
+		}
+		this.JOB_NAME = jOB_NAME3;
+		this.TRIGGER_NAME = tRIGGER_NAME3;
+		this.JOB_GROUP_NAME = jOB_GROUP_NAME3;
+		this.TRIGGER_GROUP_NAME = tRIGGER_GROUP_NAME3;
+	}
+
+	/**
+	 * 代表每多少秒循环一次
+	 * 
+	 * @param jobClass
+	 *            job的class
+	 * @param second
+	 *            秒数
+	 */
+	public void addJob(Class jobClass, int second) {
+		this.addJob(JOB_NAME, jobClass, second);
+	}
+
+	public void addJob(String jOB_NAME2, Class jobClass, int second) {
+		String hashName = jOB_NAME2 + getHash(jOB_NAME2);
+		this.addJob(jOB_NAME2, hashName, hashName, hashName, jobClass, second);
+	}
+
+	public void addJob(String jOB_NAME2, String TRIGGER_NAME, String JOB_GROUP_NAME, String TRIGGER_GROUP_NAME,
+			Class jobClass, int second) {
+		QuartzManager.addJob(jOB_NAME2, TRIGGER_NAME, JOB_GROUP_NAME, TRIGGER_GROUP_NAME, jobClass,
+				"0/" + second + " * * * * ?");
+	}
+
+	/**
+	 * 删除job
+	 */
+	public void removeJob() {
+		this.removeJob(JOB_NAME);
+	}
+
+	public void removeJob(String JOB_NAME) {
+		String hashName = JOB_NAME + getHash(JOB_NAME);
+		this.removeJob(JOB_NAME, hashName, hashName, hashName);
+	}
+
+	public void removeJob(String jOB_NAME, String jOB_GROUP_NAME, String tRIGGER_NAME, String tRIGGER_GROUP_NAME) {
+		QuartzManager.removeJob(jOB_NAME, jOB_GROUP_NAME, tRIGGER_NAME, tRIGGER_GROUP_NAME);
+	}
+
+	/**
+	 * 修改job的轮询秒数
+	 * 
+	 * @param second
+	 */
+	public void update(int second) {
+		this.updatebyName(JOB_NAME, second);
+	}
+
+	public void updatebyName(String JobName, int second) {
+		String hashName = JobName + getHash(JobName);
+		this.updatebyName(JobName, hashName, hashName, hashName, second);
+	}
+
+	public void updatebyName(String jobName, String jOB_GROUP_NAME, String tRIGGER_NAME, String tRIGGER_GROUP_NAME,
+			int second) {
+		QuartzManager.modifyJobTime(jobName, jOB_GROUP_NAME, tRIGGER_NAME, tRIGGER_GROUP_NAME,
+				"0/" + second + " * * * * ?");
+	}
+
+	public int getHash(String JobName) {
+		int m = 1 << 10 - 1;
+		return hash(JobName.hashCode()) & m;
+	}
+
+	/**
+	 * 自定义hash算法
+	 * 
+	 * @param hashCode
+	 * @return
+	 */
+	private int hash(int hashCode) {
+		hashCode = hashCode ^ (hashCode >>> 12) ^ (hashCode >>> 8);
+		return hashCode ^ (hashCode >>> 7) ^ (hashCode >>> 5);
+	}
+}
---
+++ src/test/java/rain/dsys/TestQuartz.java
@@ -0,0 +1,27 @@
+package rain.dsys;
+
+import java.util.concurrent.TimeUnit;
+
+import rain.dsys.common.QuartzJobs.MyJob1;
+import rain.dsys.common.QuartzJobs.MyJob2;
+import rain.dsys.common.QuartzUtil.QuartzUtil;
+
+public class TestQuartz {
+
+	public static void main(String[] args) throws InterruptedException {
+		// 1)初始化工具类Joe
+		QuartzUtil q = new QuartzUtil();
+		// 2)新增俩个job,并且启动【建议采取第一种写法，命名job名称，名字冲突会报错，后者的写法只能使用一次】
+		// q.addJob("JOE",MyJob1.class, 3);
+		q.addJob(MyJob2.class, 5);
+		TimeUnit.SECONDS.sleep(10);
+		// 3）修改job轮询时间
+		// q.updatebyName("JOE", 1);
+		q.update(1);
+		TimeUnit.SECONDS.sleep(10);
+		System.out.println(">>>>>>>>>>>>>>>>>");
+		// 4)删除job
+		// q.removeJob("JOE");
+		q.removeJob();
+	}
+}
