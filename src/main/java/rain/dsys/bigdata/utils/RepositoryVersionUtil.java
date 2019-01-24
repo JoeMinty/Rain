aa5a24786af63b54aaa11cd475bd948f47e0016a
joe <394054470@qq.com>
2019-01-25 00:03:09 +0800

    调整结构

--- src/main/java/rain/dsys/bigdata/utils/RepositoryVersionUtil.java
+++ src/main/java/rain/dsys/bigdata/utils/RepositoryVersionUtil.java
@@ -1,4 +1,6 @@
-package rain.dsys.common.utils;
+package rain.dsys.bigdata.utils;
+
+import com.alibaba.druid.VERSION;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -6,11 +8,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
- * 文件操作通用类
+ * 版本仓库操作通用类
  *
  * @author Joe
  */
-public class FileUtil {
+public class RepositoryVersionUtil {
 
     /**
      * 检查是否是目录
@@ -24,8 +26,20 @@ public class FileUtil {
      * 返回目录中对应最大的版本
      */
     public static Integer getMaxVersion(String path) throws IOException {
-        return Files.list(Paths.get(path)).filter(FileUtil::checkDir)
+        return Files.list(Paths.get(path)).filter(RepositoryVersionUtil::checkDir)
                 .map(Path::getFileName).map(Path::toString).mapToInt(version -> Integer.parseInt(version)).max().getAsInt();
+    }
+
+
+    /**
+     * 生成本地仓库新版本目录
+     *
+     * @param versionId  本地仓库版本号
+     */
+    public static void genLocalRepository(int versionId) {
+        // 上版本解压出来的目录
+
+        // 当前版本所做的增删改操作目录
     }
 
 }
