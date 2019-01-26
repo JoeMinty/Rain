5ea30a2c7fa057090a1beadcdd8ba3ba956e8482
joe <394054470@qq.com>
2019-01-26 23:23:05 +0800

    hdfs util

---
+++ src/main/java/rain/dsys/bigdata/utils/HdfsUtil.java
@@ -0,0 +1,15 @@
+package rain.dsys.bigdata.utils;
+
+/**
+ * hdfs 工具类
+ */
+public class HdfsUtil {
+
+    public static void copyFile(Configuration conf , String uri , String local, String remote) throws IOException {
+        FileSystem fs = FileSystem.get(URI.create(uri), conf);
+        fs.copyFromLocalFile(new Path(local), new Path(remote));
+        System.out.println("copy from: " + local + " to " + remote);
+        fs.close();
+    }
+
+}
