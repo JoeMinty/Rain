db922d374796a1bc5ad909cf5689faa4291d90f4
Joe <geekjoechan@gmail.com>
2019-04-13 18:25:46 +0800

    Create CrossDomainSupportConfig.java

---
+++ src/main/java/rain/dsys/common/config/CrossDomainSupportConfig.java
@@ -0,0 +1,20 @@
+package rain.dsys.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+      .allowedOrigins("*")
+      .allowedMethods("*")
+      .allowedHeaders("*")
+      .maxAge(3600);
+  }
+
+}
