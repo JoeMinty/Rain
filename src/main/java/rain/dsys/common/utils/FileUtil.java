168e0475571d62b9e830420363e661b0508c7e67
Joe <geekjoechan@gmail.com>
2019-04-15 22:33:44 +0800

    Update FileUtil.java

--- src/main/java/rain/dsys/common/utils/FileUtil.java
+++ src/main/java/rain/dsys/common/utils/FileUtil.java
@@ -6,4 +6,29 @@ package rain.dsys.common.utils;
  * @author Joe
  */
 public class FileUtil {
+
+   public static boolean breakPoinkCheck(int chunks, int chunk, String uploadFile) throws IOException {
+    String fileName = "aa";
+    File tempFile = new File(uploadFile, fileName + ".conf");
+    RandomAccessFile accessConfFile = null;
+    byte isComplete = Byte.MAX_VALUE;
+    try {
+      accessConfFile = new RandomAccessFile(tempFile, "rw");
+      if (accessConfFile.length() == 0)
+        accessConfFile.setLength(chunks);
+
+      accessConfFile.seek(chunk);
+      accessConfFile.write(Byte.MAX_VALUE);
+      byte[] completeList = FileUtils.readFileToByteArray(tempFile);
+      for (int i = 0; i < completeList.length && isComplete == Byte.MAX_VALUE; i++) {
+        isComplete = (byte) (isComplete & completeList[i]);
+      }
+    } finally {
+      if (accessConfFile != null) {
+        accessConfFile.close();
+      }
+    }
+
+    return isComplete == Byte.MAX_VALUE ? true : false;
+  }
 }
